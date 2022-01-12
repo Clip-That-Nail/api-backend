a22764f212a86d9333a668ea2056667438279d62
Pawel Bak <pawelbak300589@gmail.com>
2022-01-12 19:09:20 +0000

    Create user model

--- models/user.py
+++ models/user.py
@@ -2,26 +2,53 @@ from typing import Dict, Union
 
 from db import db
 
-UserJSON = Dict[str, Union[int, str]]
+UserJSON = Dict[str, Union[int, str, bool]]
 
 
 class UserModel(db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True)
+    group_id = db.Column(db.Integer, db.ForeignKey('group.id'), nullable=False)
+    first_name = db.Column(db.String(80), nullable=False)
+    last_name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80))
+    must_change_password = db.Column(db.Boolean, default=False)
+    status = db.Column(db.Integer, nullable=False, default=0)
+    created_at = db.Column(db.DateTime, nullable=False)
+    updated_at = db.Column(db.DateTime, nullable=False)
 
-    def __init__(self, username: str, password: str):
-        self.username = username
+    pets = db.relationship('PetModel', backref='user', lazy=True)
+
+    def __init__(self, group_id: int, first_name: str, last_name: str, email: str, password: str, must_change_password: bool, status: int, created_at: str, updated_at: str):
+        self.group_id = group_id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
         self.password = password
+        self.must_change_password = must_change_password
+        self.status = status
+        self.created_at = created_at
+        self.updated_at = updated_at
 
     def json(self) -> UserJSON:
-        return {"id": self.id, "username": self.username}
+        return {
+            "id": self.id,
+            "group_id": self.group_id,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "email": self.email,
+            "password": self.password,
+            "must_change_password": self.must_change_password,
+            "status": self.status,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at
+        }
 
     @classmethod
-    def find_by_username(cls, username: str) -> "UserModel":
-        return cls.query.filter_by(username=username).first()
+    def find_by_email(cls, email: str) -> "UserModel":
+        return cls.query.filter_by(email=email).first()
 
     @classmethod
     def find_by_id(cls, _id: int) -> "UserModel":
