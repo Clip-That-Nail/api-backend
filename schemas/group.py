93f1e3e4b49fe8d9dd91ada30b4c599cfe25536f
Pawel Bak <pawelbak300589@gmail.com>
2022-01-24 19:56:50 +0000

    Create group schema

---
+++ schemas/group.py
@@ -0,0 +1,15 @@
+from ma import ma
+from models.group import GroupModel
+from models.user import UserModel # just to load user
+from schemas.user import UserSchema
+
+
+class GroupSchema(ma.SQLAlchemyAutoSchema):
+    users = ma.Nested(UserSchema, many=True)
+
+    class Meta:
+        model = GroupModel
+        load_instance = True
+        # load_only = ()
+        dump_only = ("id", "created_at", "updated_at",)
+        include_fk = True
