961a62df90d09fecdc34af735e2866952435697f
Pawel Bak <pawelbak300589@gmail.com>
2022-01-27 19:14:39 +0000

    Update user resources after creating marshmallow user schema

--- resources/user.py
+++ resources/user.py
@@ -1,5 +1,6 @@
-from flask_restful import Resource, reqparse
-from werkzeug.security import check_password_hash
+from flask import request
+from flask_restful import Resource
+from werkzeug.security import check_password_hash, generate_password_hash
 from flask_jwt_extended import (
     create_access_token,
     create_refresh_token,
@@ -9,54 +10,39 @@ from flask_jwt_extended import (
 )
 from datetime import datetime
 from models.user import UserModel
+from schemas.user import UserSchema
 from blocklist import BLOCKLIST
 
-BLANK_ERROR = "'{}' cannot be blank."
 USER_ALREADY_EXISTS = "A user with that email already exists."
 CREATED_SUCCESSFULLY = "User created successfully."
+ERROR_REGISTERING = "An error occurred while registering a new user."
 USER_NOT_FOUND = "User not found."
 USER_DELETED = "User deleted."
 INVALID_CREDENTIALS = "Invalid credentials!"
 USER_LOGGED_OUT = "User <id={}> successfully logged out."
 
+user_schema = UserSchema()
+
 
 class UserRegister(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument(
-        "group_id", type=int, required=True, help=BLANK_ERROR.format("group_id")
-    )
-    parser.add_argument(
-        "first_name", type=str, required=True, help=BLANK_ERROR.format("first_name")
-    )
-    parser.add_argument(
-        "last_name", type=str, required=True, help=BLANK_ERROR.format("last_name")
-    )
-    parser.add_argument(
-        "email", type=str, required=True, help=BLANK_ERROR.format("email")
-    )
-    parser.add_argument(
-        "password", type=str, required=True, help=BLANK_ERROR.format("password")
-    )
-
     @classmethod
     def post(cls):
-        data = cls.parser.parse_args()
+        user_json = request.get_json()
 
-        if UserModel.find_by_email(data["email"]):
+        if UserModel.find_by_email(user_json["email"]):
             return {"message": USER_ALREADY_EXISTS}, 400
 
-        user = UserModel(
-            data["group_id"],
-            data["first_name"],
-            data["last_name"],
-            data["email"],
-            data["password"],
-            False,
-            0,
-            datetime.now(),
-            datetime.now()
-        )
-        user.save_to_db()
+        user_json["password"] = generate_password_hash(user_json["password"])
+        user_json["must_change_password"] = False
+        user_json["status"] = 0
+        user_json["created_at"] = datetime.now()
+        user_json["updated_at"] = datetime.now()
+        user = user_schema.load(user_json)
+
+        try:
+            user.save_to_db()
+        except:
+            return {"message": ERROR_REGISTERING}, 500
 
         return {"message": CREATED_SUCCESSFULLY}, 201
 
@@ -67,10 +53,13 @@ class User(Resource):
         user = UserModel.find_by_id(user_id)
         if not user:
             return {"message": USER_NOT_FOUND}, 404
-        return user.json(), 200
+        return user_schema.dump(user), 200
 
     @classmethod
+    @jwt_required(refresh=True)
     def delete(cls, user_id: int):
+        # TODO: check if user using it is an Admin
+        # user_id = get_jwt_identity()
         user = UserModel.find_by_id(user_id)
         if not user:
             return {"message": USER_NOT_FOUND}, 404
@@ -79,21 +68,13 @@ class User(Resource):
 
 
 class UserLogin(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument(
-        "email", type=str, required=True, help=BLANK_ERROR.format("email")
-    )
-    parser.add_argument(
-        "password", type=str, required=True, help=BLANK_ERROR.format("password")
-    )
-
     @classmethod
     def post(cls):
-        data = cls.parser.parse_args()
+        user_json = request.get_json()
 
-        user = UserModel.find_by_email(data["email"])
+        user = UserModel.find_by_email(user_json["email"])
 
-        if user and check_password_hash(user.password, data["password"]):
+        if user and check_password_hash(user.password, user_json["password"]):
             access_token = create_access_token(identity=user.id, fresh=True)
             refresh_token = create_refresh_token(user.id)
             return {"access_token": access_token, "refresh_token": refresh_token}, 200
@@ -105,7 +86,8 @@ class UserLogout(Resource):
     @classmethod
     @jwt_required()
     def post(cls):
-        jti = get_jwt()["jti"]  # jti is "JWT ID", a unique identifier for a JWT.
+        # jti is "JWT ID", a unique identifier for a JWT.
+        jti = get_jwt()["jti"]
         user_id = get_jwt_identity()
         BLOCKLIST.add(jti)
         return {"message": USER_LOGGED_OUT.format(user_id)}, 200
@@ -115,6 +97,7 @@ class TokenRefresh(Resource):
     @classmethod
     @jwt_required(refresh=True)
     def post(cls):
+        # TODO: should I check blocked jti ? should user be able to refresh token that is already logged out
         current_user = get_jwt_identity()
         new_token = create_access_token(identity=current_user, fresh=False)
         return {"access_token": new_token}, 200
